d27cc2408ef8644cf79468750225a1be99b206fb
unknown <jeff.de.sa@gmail.com>
2021-11-04 12:21:31 -0300

    exercicio favorite number

---
+++ Exercicios/src/main/java/br/com/impacta/cursointroducaojavaturma6/favoriteNumber.java
@@ -0,0 +1,11 @@
+package br.com.impacta.cursointroducaojavaturma6;
+
+public class favoriteNumber {
+    public static void main(String[] args) {
+        System.out.format("My favorite number is %.3f.\n", 3.141592654);
+
+        //part of a time tables:
+        System.out.format("%2d %2d %2d\n", 4, 6, 8);
+        System.out.format("%2d %2d %2d\n", 8, 12, 16);
+    }
+}
