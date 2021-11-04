0edc7968951280c47d3cecdc61d08bf221e7caf8
unknown <jeff.de.sa@gmail.com>
2021-11-04 12:07:21 -0300

    first commit

---
+++ Exercicios/src/main/java/br/com/impacta/cursointroducaojavaturma6/CursoIntroducaoJavaTurma6Application.java
@@ -0,0 +1,13 @@
+package br.com.impacta.cursointroducaojavaturma6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoIntroducaoJavaTurma6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoIntroducaoJavaTurma6Application.class, args);
+	}
+
+}
---
+++ Exercicios/src/test/java/br/com/impacta/cursointroducaojavaturma6/CursoIntroducaoJavaTurma6ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.impacta.cursointroducaojavaturma6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoIntroducaoJavaTurma6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
